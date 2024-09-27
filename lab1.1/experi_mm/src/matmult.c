878de9176760ed4fed426291f46d7a41f3c15a91
Albresky <albre02@outlook.com>
2024-09-27 06:42:40 +0000

    [Lab1.1] MM 优化 2（作弊🤣）: 使用 partition 将 IO 的 array完全分解到寄存器级别

--- lab1.1/experi_mm/src/matmult.c
+++ lab1.1/experi_mm/src/matmult.c
@@ -22,7 +22,28 @@
 } */
 
 //////////////// [Op1: Use unroll pragma to minimize loop times] ////////////////
+/* void matmult(short A[N][N], short B[N][N], short C[N][N]){
+	short i, j, k;
+    short sum = 0;
+    
+	for (i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            for(k = 0; k < N; k++){
+#pragma HLS unroll factor=16                
+                sum += A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+            sum = 0;
+        }
+    }
+} */
+
+
+//////////////// [Op2: Use patition pragma to initialize array into registers] ////////////////
 void matmult(short A[N][N], short B[N][N], short C[N][N]){
+#pragma HLS array_partition variable=A type=complete dim=0
+#pragma HLS array_partition variable=B type=complete dim=0
+#pragma HLS array_partition variable=C type=complete dim=0
 	short i, j, k;
     short sum = 0;
     
