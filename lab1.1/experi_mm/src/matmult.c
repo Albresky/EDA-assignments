0fb059cc062e395087fa32ea9f0d790971721a77
Albresky <albre02@outlook.com>
2024-09-27 03:02:56 +0000

    [Lab1.1] MM 优化1: 使用 unroll pragma 展开最内层循环

--- lab1.1/experi_mm/src/matmult.c
+++ lab1.1/experi_mm/src/matmult.c
@@ -4,6 +4,24 @@
 
 #include "matmult.h"
 
+
+//////////////// Original Code ////////////////
+/* void matmult(short int A[N][N],short int B[N][N],int C[N][N]){
+	int i, j, k;
+    int sum = 0;
+    
+	for (i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            for(k = 0; k < N; k++){
+                sum += A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+            sum = 0;
+        }
+    }
+} */
+
+//////////////// [Op1: Use unroll pragma to minimize loop times] ////////////////
 void matmult(short int A[N][N],short int B[N][N],int C[N][N]){
 	int i, j, k;
     int sum = 0;
@@ -11,6 +29,7 @@ void matmult(short int A[N][N],short int B[N][N],int C[N][N]){
 	for (i = 0; i < N; i++){
         for(j = 0; j < N; j++){
             for(k = 0; k < N; k++){
+#pragma HLS unroll factor=16                
                 sum += A[i][k] * B[k][j];
             }
             C[i][j] = sum;
